7bba3905d9127ecf4a5b577b6ae138da8119a4f2
andrtechno <andrew.panix@gmail.com>
2017-09-19 13:43:01 +0300

    init

--- TinyMce.php
+++ TinyMce.php
@@ -64,7 +64,7 @@ class TinyMce extends InputWidget {
         $this->clientOptions['plugins'] = [
             "advlist autolink lists link charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
+            "insertdatetime media table contextmenu paste pagebreak"
         ];
         $this->clientOptions['toolbar'] = "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image";
 
